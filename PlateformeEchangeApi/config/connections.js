940ddd7885959a4648af751bf8bfcace2d7da924
G Q <G Q>
2017-01-06 12:03:19 +0100

    Database settings

---
+++ PlateformeEchangeApi/config/connections.js
@@ -0,0 +1,9 @@
+module.exports.connections = {
+ apiSocieteTransport: {
+    adapter: 'sails-mysql',
+    host: 'localhost',
+    user: 'root',
+    password: 'onestdesoufs',
+    database: 'apisocietetransport'
+  }
+};
