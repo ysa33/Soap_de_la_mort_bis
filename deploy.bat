47ad3c190c69166dbfab79f15a96eaa7d6d879e1
G Q <G Q>
2017-01-13 15:47:43 +0100

    Update deployment script

--- deploy.bat
+++ deploy.bat
@@ -3,6 +3,6 @@
 
 start cmd /k node PlateformeEchangeApi\app.js
 start cmd /k node Supervision\server.js
-
-start cmd /k "cd Technicien & ionic serve -p 8101"
-start cmd /k "cd FrontConducteur & ionic serve"
+ 
+start cmd /k "cd Technicien & ionic serve -p 8101 -i 35730"
+start cmd /k "cd FrontConducteur & ionic serve -p 8100 -i 35729"
